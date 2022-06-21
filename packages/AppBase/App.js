11e30faf36e38aa7fd2b862b534cdfd79c3a43fd
QUYNH999 <huongquynh99uel@gmail.com>
2022-06-21 22:47:43 +0700

    Lerna SuperApp MonoRepo

---
+++ packages/AppBase/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {View, Text} from "react-native";
+import Home from 'home'
+
+export default function App() {
+  return(
+      <View style={{flex: 1}}>
+        {/*<Text>hello</Text>*/}
+          <Home/>
+      </View>
+  )
+}
